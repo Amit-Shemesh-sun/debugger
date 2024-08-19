556b1d1d405b12bba8af0b0db4cfe3deddd7b3d3
Amit Shemesh <cocken.bolstuchure@gmail.com>
2024-08-19 11:10:22 +0300

    Add define statement for CLI options

--- sdb.c
+++ sdb.c
@@ -4,11 +4,24 @@
 #include "sdb.h"
 #include <sys/wait.h>
 #include <string.h>
+#include "commands.h"
 
 
 #define CONTINUE ("continue")
 #define QUIT ("q")
 #define BREAK ("break")
+#define PRINT ("print")
+
+#define OPTION_NO_COMMAND(string, function) \
+        do {\
+            if (!strncmp(string , command, sizeof(string) - 1)) {\
+                function(pid); } }\
+        while (0)
+#define OPTION_COMMAND(string, function) \
+        do {\
+            if (!strncmp(string , command, sizeof(string) - 1)) {\
+                function(pid, command); } } \
+        while (0)
 
 void run_debugee(char* program_name) {
     if (ptrace(PTRACE_TRACEME, 0, 0, 0) < 0) {
@@ -24,30 +37,13 @@ void run_debuger(int pid, char* program_name) {
 
     waitpid(pid, &wait_status, options);
     while(1) {
-        printf("sdb>");
+        printf("sdb> ");
         fgets(command, sizeof(command), stdin);
-        if (!strncmp(CONTINUE, command, sizeof(CONTINUE) - 1)) {
-            continue_program(pid);
-        }
-        else if (!strncmp(BREAK, command, sizeof(QUIT) - 1)) {
-            set_break(pid, command);
-        }
-        else if (!strncmp(QUIT, command, sizeof(QUIT) - 1)) {
+        if (!strncmp(QUIT, command, sizeof(QUIT) - 1)) {
             return;
-        }
+        };
+        OPTION_NO_COMMAND(CONTINUE, continue_program);
+        OPTION_COMMAND(BREAK, set_break);
+        OPTION_COMMAND(PRINT, print);
     }
-}
-
-
-void continue_program(int pid) {
-    int wait_status;
-    int options = 0;
-
-    ptrace(PTRACE_CONT, pid, NULL, NULL);
-    waitpid(pid, &wait_status, options);
-}
-
-void set_break(int pid, char command[]) {
-    char token[]  = strtok(strtok(command, ' '), ' ');
-    
 }
--- sdb.h
+++ sdb.h
@@ -3,6 +3,3 @@ void run_debugee(char* program_name);
 
 // debugger note
 void run_debuger(int pid, char* programm_name);
-
-// hello
-void continue_program(int pid);
